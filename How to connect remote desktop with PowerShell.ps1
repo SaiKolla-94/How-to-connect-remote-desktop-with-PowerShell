3eccbbc35c71195c4cd96a86f1c0dae6f9f85b00
SaiKolla <56868273+SaiKolla-94@users.noreply.github.com>
2020-10-03 16:52:00 +0530

    Add files via upload

---
+++ How to connect remote desktop with PowerShell.ps1
@@ -0,0 +1,20 @@
+﻿#Source System:
+
+#Remote Enable
+
+Enable-PSRemoting -Force
+
+#Remote enable status
+
+Get-PSSessionConfiguration | Format-Table -Property Name, Permission
+
+#Destinetion System:
+
+#Source remote test
+
+Test-WSMan 10.64.133.110
+
+#Connecr remote server
+
+Enter-PSSession ABCDEFGH (HostName) -Credential UserName@Domain
+#EX:Saikolla@Sai1994.com
